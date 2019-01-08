3fc580048f9e0882580578995bc9b59dc7f340d8
TranHungQuang <tranhunquang23497@gmail.com>
2019-01-08 14:49:57 +0700

    update

--- FinalProjectAnalytics/view/js/index.js
+++ FinalProjectAnalytics/view/js/index.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	var swiper = new Swiper('.swiper-container', {
       slidesPerView: 3,
       spaceBetween: 30,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
